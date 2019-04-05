e99edc9b076d34ef334a1f151582bc35c526bd96
colinwang117 <colinwang117@foxmail.com>
2019-04-05 20:54:49 +0800

    添加 tabs 相关组件，添加基本 props

---
+++ src/tabs-body.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tabs-body">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RainbowTabsBody'
+  }
+</script>
+
+<style>
+  .tabs-body {
+  }
+</style> 
---
+++ src/tabs-head.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs-head">
+    <slot></slot>
+    <slot name="actions"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RainbowTabsHead'
+  }
+</script>
+
+<style>
+  .tabs-head {
+  }
+</style> 
