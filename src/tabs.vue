c8e9f045c9e501ea57a7e438ccd06195952e7f19
colinwang117 <colinwang117@foxmail.com>
2019-04-09 12:04:06 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs.vue
+++ src/tabs.vue
@@ -32,9 +32,19 @@ export default {
     }
   },
   mounted() {
-    // this.$emit('update:selected', '这是 this $emit 出来的数据')
-    this.eventBus.$emit('update:selected', this.selected)
-    // // this.$emit('update:selected', 'xxx')
+    this.$children.forEach(vm => {
+      if (vm.$options.name === 'RainbowTabsHead') {
+        vm.$children.forEach(childVm => {
+          if (
+            childVm.$options.name === 'RainbowTabsItem' &&
+            childVm.name === this.selected
+          ) {
+            // this.$emit('update:selected', '这是 this $emit 出来的数据')
+            this.eventBus.$emit('update:selected', this.selected, childVm)
+          }
+        })
+      }
+    })
   }
 }
 </script>
